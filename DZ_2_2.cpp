c70ab6da1cd7e1c699de8367f3cefd85c955b223
Ноутбук <@NOOTBOOK>
2026-01-16 22:01:10 +0300

    Добавьте файлы проекта.

---
+++ DZ_2_2.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::string str;
+    std::cout << "Enter your word: \n";
+    std::cin >> str;
+    std::cout << "You entered: \n" << str << std::endl;
+}
+
